095776c40fcb480d1db9b8b7bb7e95116107851e
Gabriel rieff <gabeerieff@gmail.com>
2023-06-13 23:06:08 -0300

    add tag p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello world!</h1>
+      <p>Ajustes na branch</p>
     </main>
   );
 }
